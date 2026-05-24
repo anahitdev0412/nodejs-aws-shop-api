5ede76fc388fad400e2b0b9773e22e8038a4f51d
anahit <anahit.meghri@gmail.com>
2026-05-24 22:45:06 +0200

    Added possibility to generate presignedURl.

--- infrastructure/lib/import_product_stack.ts
+++ infrastructure/lib/import_product_stack.ts
@@ -7,16 +7,62 @@ import { S3Construct } from "./constructs/s3_construct";
 import { ApiGatewayConstruct } from "./constructs/api_gateway_construct";
 import * as apigwv2 from "aws-cdk-lib/aws-apigatewayv2";
 
+export interface ImportApiStackProps extends cdk.StackProps {
+  envName: string;
+  enableTracing?: boolean;
+}
+
 const MONOREPO_ROOT = path.resolve(__dirname, "../../");
 
 export class ImportProductStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ImportApiStackProps) {
     super(scope, id, props);
+
+    const { envName, enableTracing = false } = props;
+
+    const sharedLambdaProps = { envName, enableTracing };
 
     // S3 Bucket for CSV uploads
     const s3BucketforCSV = new S3Construct(this, "S3CSV", {
       envName: this.node.tryGetContext("env") || "dev",
     });
 
+    const importProductsFile = new LambdaConstruct(this, "ImportProductsFile", {
+      ...sharedLambdaProps,
+      functionName: "importProductsFile",
+      lambdaPackagePath: path.join(
+        MONOREPO_ROOT,
+        "import_service/lambdas/import_products_file"
+      ),
+      entry: path.join(
+        MONOREPO_ROOT,
+        "import_service/lambdas/import_products_file/src/index.ts",
+      ),
+      description: "Generates presigned S3 URL for CSV upload (GET /import)",
+      environment: {
+        BUCKET_NAME: s3BucketforCSV.bucket.bucketName,
+        UPLOADED_FOLDER: "uploaded",
+      },
+    });
+
+    s3BucketforCSV.bucket.grantReadWrite(importProductsFile.lambdaFunction);
+
+    const gateway = new ApiGatewayConstruct(this, "ImportApiGateway", {
+      envName,
+      apiName: "import-api",
+      routes: [
+        {
+          path: "/import",
+          method: apigwv2.HttpMethod.GET,
+          fn: importProductsFile.lambdaFunction,
+          integrationId: "ImportProductsFileIntegration",
+        },
+      ],
+    });
+
+    new cdk.CfnOutput(this, "ImportBucketName", {
+      value: s3BucketforCSV.bucket.bucketName,
+      description: "Import S3 bucket name",
+    });
   }
 }
